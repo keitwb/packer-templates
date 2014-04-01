f216117e14ba519c1e6c257cfc2c5dbb3be9b0a5
Ben Keith <keitwb@gmail.com>
2014-03-31 22:30:07 -0400

    adding debian-wheezy and updating ubuntu

--- debian-wheezy/scripts/vm_cleanup.sh
+++ debian-wheezy/scripts/vm_cleanup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 # Removing leftover leases and persistent rules
 echo "cleaning up dhcp leases"
@@ -6,25 +6,29 @@ rm /var/lib/dhcp/*
 
 # Make sure Udev doesn't block our network
 echo "cleaning up udev rules"
-rm /etc/udev/rules.d/70-persistent-net.rules
-mkdir /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
 rm /lib/udev/rules.d/75-persistent-net-generator.rules
 
 echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
 echo "pre-up sleep 2" >> /etc/network/interfaces
 
+sed -i 's/timeout=5/timeout=1/' /boot/grub/grub.cfg
+
 # Zero out the free space to save space in the final image:
-#echo "Zeroing device to make space..."
-#aptitude -y install zerofree
-#ROOT_FS=$(mount | grep 'on / type' | awk '{ print $1 }')
-
-## Kill running services
-#fuser -m $ROOT_FS -k -w
-
-#mount -oremount,ro $ROOT_FS
-#zerofree $ROOT_FS
-#mount -oremount,rw $ROOT_FS
-#service ssh start
-
+echo "Zeroing device to make space..."
+aptitude -y install zerofree psmisc
 apt-get clean
+
+ROOT_FS=$(mount | grep 'on / type' | awk '{ print $1 }')
+## Kill running services
+echo 'Killing some processes...'
+pkill rpc
+pkill dhclient
+pkill rsyslog
+pkill VBoxService
+sleep 5s 
+echo 'Remounting readonly...'
+mount -oremount,ro $ROOT_FS
+echo 'Running zerofree...'
+zerofree $ROOT_FS
+
